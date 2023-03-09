b053c365e6d5f497929788cdb60eb24626815385
caioarruda <caioarruda@gmail.com>
2023-03-09 14:57:02 -0300

    server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import Routers from './Routes/index.js'
+import Routers from './Routes/Index.js'
 import { config } from 'dotenv'
 import dbConnect from './Config/dbConfig.js'
 import userContextInstance from './Utils/UserContext.js'
